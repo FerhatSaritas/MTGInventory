6874a874eb8d253bde30370769220a4a557e8dfd
Ferhat Saritas <ferhat.saritas@campus.tu-berlin.de>
2019-11-14 13:21:12 +0100

    Slight update to table

--- mtginventory/inventory/views.py
+++ mtginventory/inventory/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Card
+from .models import Card, Player, States, Colour, Expansion
 import logging as logger
 # Create your views here.
 
 def index(request):
     template = loader.get_template('inventory/index.html')
     objects = Card.objects.all()
+    items=list()
+
+    objects = [card.__dict__ for card in objects]
+    for obj in objects:
+        tmp = dict()
+        tmp = obj
+        owner = Player.objects.get(id=tmp["owner_id"])
+        state = States.objects.get(id=tmp["state_of_card_id"])
+        expansion = Expansion.objects.get(id=tmp["set_name_id"])
+        colour = Colour.objects.get(id=tmp["colour_id"])
+        tmp["owner_id"] = owner.name
+        tmp["state_of_card_id"] = state.state
+        tmp["set_name_id"] = expansion.name
+        tmp["colour_id"] = colour.colour
+        items.append(tmp)
     
-    header = [card.__dict__ for card in objects]
-    header = [i for i in header[0]][1:]
+    header = ["ID", "Name", "Set", "CMC", "#Karten", "Kartenzustand", "Besitzer", "Farbe/-n"]
     
     logger.warning(header)
-    return render(request, 'inventory/index.html', {'header':header, 'rows':objects})
+    return render(request, 'inventory/index.html', {'header':header, 'rows':items})
